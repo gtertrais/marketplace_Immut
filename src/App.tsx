114347f1d6a46f32b2c4807dcc7b96ebea305ce1
gaspard <gaspard.tertrais@gmail.com>
2021-10-16 22:38:22 +0200

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,124 @@
 import './App.css';
 import { ImmutableXClient } from '@imtbl/imx-sdk';
 import { Link } from '@imtbl/imx-sdk';
-import { ImmutableMethodResults} from '@imtbl/imx-sdk';
+import { ImmutableMethodResults } from '@imtbl/imx-sdk';
 import { useEffect, useState } from 'react';
 import Marketplace from './Marketplace';
 import Inventory from './Inventory';
 import Bridging from './Bridging';
+import Coins from './Coins';
+import { Card, ListGroup, ListGroupItem, Row, Col, Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
+
 require('dotenv').config();
 
 const App = () => {
-  // initialise Immutable X Link SDK
-  const link = new Link(process.env.REACT_APP_ROPSTEN_LINK_URL)
-  
-  // general
-  const [tab, setTab] = useState('marketplace');
-  const [wallet, setWallet] = useState('undefined');
-  const [balance, setBalance] = useState<ImmutableMethodResults.ImmutableGetBalanceResult>(Object);
-  const [client, setClient] = useState<ImmutableXClient>(Object);
+	// initialise Immutable X Link SDK
+	const link = new Link(process.env.REACT_APP_ROPSTEN_LINK_URL)
+
+	// general
+	const [tab, setTab] = useState('marketplace');
+	const [wallet, setWallet] = useState('undefined');
+	const [balance, setBalance] = useState<ImmutableMethodResults.ImmutableGetBalanceResult>(Object);
+	const [client, setClient] = useState<ImmutableXClient>(Object);
+	const [show, setShow] = useState(false);
 
 
-  useEffect(() => {
-    buildIMX()
-  }, [])
+	useEffect(() => {
+		buildIMX()
+	}, [])
 
-  // initialise an Immutable X Client to interact with apis more easily
-  async function buildIMX() {
-    const publicApiUrl: string = process.env.REACT_APP_ROPSTEN_ENV_URL ?? '';
-    setClient(await ImmutableXClient.build({publicApiUrl}))
-  }
+	// initialise an Immutable X Client to interact with apis more easily
+	async function buildIMX() {
+		const publicApiUrl: string = process.env.REACT_APP_ROPSTEN_ENV_URL ?? '';
+		setClient(await ImmutableXClient.build({ publicApiUrl }))
+	}
 
-  // register and/or setup a user
-  async function linkSetup(): Promise<void> {
-    const res = await link.setup({})
-    setWallet(res.address)
-    setBalance(await client.getBalance({user: res.address, tokenAddress: 'eth'}))
-  };
+	// register and/or setup a user
+	async function linkSetup(): Promise<void> {
+		const res = await link.setup({})
+		setWallet(res.address)
+		setBalance(await client.getBalance({ user: res.address, tokenAddress: 'eth' }))
+	};
 
-  function handleTabs() {
-    if (client.address) {
-      switch (tab) {
-        case 'inventory':
-          if (wallet === 'undefined') return <div>Connect wallet</div>
-          return <Inventory
-            client={client}
-            link={link}
-            wallet={wallet}
-          />
-        case 'bridging':
-          if (wallet === 'undefined') return <div>Connect wallet</div>
-          return <Bridging
-            client={client}
-            link={link}
-            wallet={wallet}
-          />
-        default:
-          return <Marketplace
-            client={client}
-            link={link}
-          />
-      }
-    }
-    return null
-  }
+	function handleTabs() {
+		if (client.address) {
+			switch (tab) {
+				case 'inventory':
+					if (wallet === 'undefined') return <div>Connect wallet</div>
+					return <Inventory
+						client={client}
+						link={link}
+						wallet={wallet}
+					/>
+				case 'bridging':
+					if (wallet === 'undefined') return <div>Connect wallet</div>
+					return <Bridging
+						client={client}
+						link={link}
+						wallet={wallet}
+					/>
+				case 'coins':
+					if (wallet === 'undefined') return <div>Connect wallet</div>
+					return <Coins
+						client={client}
+						link={link}
+						wallet={wallet}
+					/>
+				default:
+					return <Marketplace
+						client={client}
+						link={link}
+					/>
+			}
+		}
+		return null
+	}
 
-  return (
-    <div className="App">
-      <button onClick={linkSetup}>Setup</button>
-      <div>
-        Active wallet: {wallet}
-      </div>
-      <div>
-        ETH balance (in wei): {balance?.balance?.toString()}
-      </div>
-      <button onClick={() => setTab('marketplace')}>Marketplace</button>
-      <button onClick={() => setTab('inventory')}>Inventory</button>
-      <button onClick={() => setTab('bridging')}>Deposit and withdrawal</button>
-      <br/><br/><br/>
-      {handleTabs()}
-    </div>
-  );
+	const showDropdown = (e) => {
+		setShow(!show);
+	}
+	const hideDropdown = e => {
+		setShow(false);
+	}
+
+	return (
+
+		<div className="App">
+			<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+				<Container>
+					<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+					<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+					<Navbar.Collapse id="responsive-navbar-nav">
+						<Nav className="me-auto">
+							<Nav.Link onClick={() => setTab('marketplace')}>Marketplace</Nav.Link>
+							<Nav.Link onClick={() => setTab('inventory')}>Inventory</Nav.Link>
+							<Nav.Link onClick={() => setTab('bridging')}>Deposit and withdrawal</Nav.Link>
+							<NavDropdown
+								title="Dropdown"
+								id="collasible-nav-dropdown"
+								show={show}
+								onMouseEnter={showDropdown}
+								onMouseLeave={hideDropdown}
+								style={{ width: '166px' }}>
+								<NavDropdown.Item onClick={() => setTab('coins')}>Coins</NavDropdown.Item>
+							</NavDropdown>
+						</Nav>
+						<Nav>
+							{wallet === 'undefined' &&
+								<Nav.Link onClick={linkSetup}>Setup</Nav.Link>}
+							{wallet !== 'undefined' &&
+								<>
+									<Navbar.Text >Active wallet: {wallet.replace(wallet.substring(4, 38), "...")}</Navbar.Text>
+									<Navbar.Text style={{ paddingLeft: '40px' }}>ETH balance (in wei): {balance?.balance?.toString()}</Navbar.Text>
+								</>}
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+			<br /><br /><br />
+			{handleTabs()}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Coins.tsx
@@ -0,0 +1,86 @@
+import { ethers } from 'ethers';
+import { ImmutableXClient } from '@imtbl/imx-sdk';
+import { Link } from '@imtbl/imx-sdk';
+import { MintableERC721TokenType } from '@imtbl/imx-sdk';
+import { useState } from 'react';
+import { Card, ListGroup, ListGroupItem, Row, Col, Container } from 'react-bootstrap';
+
+require('dotenv').config();
+
+interface CoinsProps {
+	client: ImmutableXClient,
+	link: Link,
+	wallet: string
+}
+
+const Coins = ({ client, link, wallet }: CoinsProps) => {
+	// minting
+	const [mintTokenId, setMintTokenId] = useState('');
+	const [mintBlueprint, setMintBlueprint] = useState('');
+
+
+	// helper function to generate random ids
+	function random()
+		: number {
+		const min = 1;
+		const max = 1000;
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+	// the minting function should be on your backend
+	async function mint() {
+		// initialise a client with the minter for your NFT smart contract
+		const provider = new ethers.providers.JsonRpcProvider(`https://eth-ropsten.alchemyapi.io/v2/${process.env.REACT_APP_ALCHEMY_API_KEY}`);
+		const minterPrivateKey: string = process.env.REACT_APP_MINTER_PK ?? ''; // registered minter for your contract
+		const minter = new ethers.Wallet(minterPrivateKey).connect(provider);
+		const publicApiUrl: string = process.env.REACT_APP_ROPSTEN_ENV_URL ?? '';
+		const starkContractAddress: string = process.env.REACT_APP_ROPSTEN_STARK_CONTRACT_ADDRESS ?? '';
+		const registrationContractAddress: string = process.env.REACT_APP_ROPSTEN_REGISTRATION_ADDRESS ?? '';
+		const minterClient = await ImmutableXClient.build({
+			publicApiUrl,
+			signer: minter,
+			starkContractAddress,
+			registrationContractAddress,
+		})
+
+		// mint any number of NFTs to specified wallet address (must be registered on Immutable X first)
+		const token_address: string = process.env.REACT_APP_TOKEN_ADDRESS ?? ''; // contract registered by Immutable
+		const result = await minterClient.mint({
+			mints: [{
+				etherKey: wallet,
+				tokens: [{
+					type: MintableERC721TokenType.MINTABLE_ERC721,
+					data: {
+						id: mintTokenId, // this is the ERC721 token id
+						blueprint: mintBlueprint, // this is passed to your smart contract at time of withdrawal from L2
+						tokenAddress: token_address.toLowerCase(),
+					}
+				}],
+				nonce: random().toString(10),
+				authSignature: ''
+			}]
+		});
+		console.log(`Token minted: ${result.results[0].token_id}`);
+	};
+
+	return (
+		<div>
+			<div>
+				Mint NFT:
+				<br />
+				<label>
+					Token ID:
+					<input type="text" value={mintTokenId} onChange={e => setMintTokenId(e.target.value)} />
+				</label>
+				<label>
+					Blueprint:
+					<input type="text" value={mintBlueprint} onChange={e => setMintBlueprint(e.target.value)} />
+				</label>
+				<button onClick={mint}>Mint</button>
+			</div>
+			<br />
+		</div>
+	);
+}
+
+export default Coins;
